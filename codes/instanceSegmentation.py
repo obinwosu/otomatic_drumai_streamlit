17251e986df58530e49ce29589de7d1bd07fc52e
obinwosu <117573177+obinwosu@users.noreply.github.com>
2023-06-14 12:04:27 -0400

    instanceSegmentation.py
    Updated code with my custom visualizer and sizing

--- codes/instanceSegmentation.py
+++ codes/instanceSegmentation.py
@@ -9,79 +9,60 @@ from detectron2.utils.visualizer import ColorMode, Visualizer
 from detectron2 import model_zoo
 from PIL import Image 
 import PIL 
-import cv2
 import numpy as np
-import tqdm
-
-
+import os, json, cv2, random
+import matplotlib.pyplot as plt
 
 class Detector:
 
     def __init__(self, model_type = "instanceSegmentation"):
         self.cfg = get_cfg()
         self.cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
-        self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+        self.cfg.MODEL.WEIGHTS = "/Users/obinwosu/otomatic/DrumAI_06102023.pth"
         self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7
         self.cfg.MODEL.DEVICE = "cpu" # cpu or cuda
 
         self.predictor = DefaultPredictor(self.cfg)
 
-    def onImage(self, imagePath):
-        image = cv2.imread(imagePath)
-        predictions = self.predictor(image)
+    def onImage(imagePath):
+        im = cv2.imread(imagePath)
+        outputs = predictor(im)
+        masks=outputs["instances"].pred_masks.to("cpu").numpy().astype('uint8')
+        areas = np.zeros(masks.shape[0])
+        i=0
+        blank = np.zeros(im.shape[0:2])
 
-        viz = Visualizer(image[:,:,::-1],metadata= MetadataCatalog.get(self.cfg.DATASETS.TRAIN[0]),scale=1.2)#instance_mode=ColorMode.IMAGE_BW)
-        
-        output = viz.draw_instance_predictions(predictions['instances'].to('cpu'))
+        for mask in masks:
+          contours = []
+          contour, _ = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+          contours.append(contour[0])
+          cnt=contour[0]
+          area = cv2.contourArea(cnt)
+          areas[i]= area
+          i = i + 1
+          if i==2:
+            ratio=np.min(areas)/np.max(areas)
+            ratio_value=ratio.item()
+            percentage="{:.1%}".format(ratio_value)
+            img1 = cv2.drawContours( blank.copy(), contours[0], 0, 1)
+            img2 = cv2.drawContours( blank.copy(), contours[0], 1, 1)
+            intersection = np.logical_and( img1, img2 )
+            marginal = (intersection.any())
+            if marginal == False:
+              x = "central perforation"
+            else:
+              x = "marginal perforation"
+            result = "The model identified a {}".format(x) + " which is {} of the total tympanic membrane".format(percentage)
+            print(result)
+
+        v = Visualizer(im[:, :, ::-1],
+                    metadata=perf_seg_metadata,
+                    scale=0.8,
+                    instance_mode=ColorMode.SEGMENTATION
+        )
+        v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
         filename = 'result.jpg'
-        cv2.imwrite(filename, output.get_image()[:,:,::-1])
-        #cv2.imshow("Result",output.get_image()[:,:,::-1])
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-
-
-    def onVideo(self,videoPath):
-        video = cv2.VideoCapture(videoPath)
-        width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
-        height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        frames_per_second = video.get(cv2.CAP_PROP_FPS)
-        num_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-
-        #fourcc = cv2.VideoWriter_fourcc(*'mpv4')
-        #out = cv2.VideoWriter("detected_video.mp4", fourcc, 20.0, (w, h))
-
-        #initializing videoWriter
-        fourcc = cv2.VideoWriter_fourcc(*'mpv4')
-        video_writer = cv2.VideoWriter("output.mp4", fourcc , fps=float(frames_per_second), frameSize=(width, height), isColor=True)
-
-       
-        v = VideoVisualizer(MetadataCatalog.get(self.cfg.DATASETS.TRAIN[0]), ColorMode.IMAGE)
-
-        def runOnVideo(video,maxFrames):# this is for debugging
-
-            readFrames = 0
-            while True:
-                hasFrame , frame = video.read()
-                if not hasFrame:
-                    break
-                outputs = self.predictor(frame)
-                
-
-                frame  = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-
-                visualization = v.draw_instance_predictions(frame, outputs['instances'].to('cpu'))
-                visualization = cv2.cvtColor(visualization.get_image(),cv2.COLOR_RGB2BGR)
-                yield visualization
-                
-                readFrames += 1
-                if readFrames > maxFrames:
-                    break
-        num_frames = 200 # here  we reinitialize the number of frames because  of it'll take hours to write the detectedVideos to our video_file and since we don't have a gpu
-        # if num_frames is not re-inititialized. the entire frames of the video will be taken into account.. usually taking hours to detect since a 'cpu' and not'gpu' is used
-        for visualization in tqdm.tqdm(runOnVideo(video, num_frames), total = num_frames):
-            #cv2.imwrite("detected_image.png", visualization)
-
-            video_writer.write(visualization)
-        video.release()
-        video_writer.release()
-        cv2.destroyAllWindows()
+        plt.figure(figsize = (22, 22))
+        plt.subplot(121);plt.imshow(im[:,:,::-1]);plt.title("Input");plt.axis('off')
+        plt.subplot(122);plt.imshow(cv2.cvtColor(v.get_image()[:, :, ::-1], cv2.COLOR_BGR2RGB));plt.title("Predicted Segmentation");plt.axis('off');
+        plt.show()
